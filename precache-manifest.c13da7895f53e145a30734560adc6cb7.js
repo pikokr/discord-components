d02ee5016f64ed4b4391bd0408afb4cb0f3e4901
ditto7890 <ditto7891@gmail.com>
2020-09-11 16:32:55 +0900

    Updates

---
+++ precache-manifest.c13da7895f53e145a30734560adc6cb7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "901e056684c673713c083ce3b00892f9",
+    "url": "/discord-components/index.html"
+  },
+  {
+    "revision": "08a59816969ad82d3925",
+    "url": "/discord-components/static/css/main.29c3e102.chunk.css"
+  },
+  {
+    "revision": "5450a454b1f0e9d3f05d",
+    "url": "/discord-components/static/js/2.8fb1e412.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/discord-components/static/js/2.8fb1e412.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08a59816969ad82d3925",
+    "url": "/discord-components/static/js/main.63e44af7.chunk.js"
+  },
+  {
+    "revision": "6f65075c71005f0febd3",
+    "url": "/discord-components/static/js/runtime-main.553e5f37.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.09b2ef7010b3b31785588f4a4bc1d2ed.js"
+  "/discord-components/precache-manifest.c13da7895f53e145a30734560adc6cb7.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/discord-components/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
